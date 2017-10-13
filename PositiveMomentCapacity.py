55afcc9a777bf3dd59a612e4a0a2c2b4b6955eed
ThomasDyhr <thomasdyhr123@hotmail.com>
2017-10-13 15:08:03 +0200

    Updated version with new inputs and outputs for design values, version 003

--- PositiveMomentCapacity.py
+++ PositiveMomentCapacity.py
@@ -5,6 +5,7 @@ FireSim made by Thomas Dyhr, DTU.BYG
 
     Args:
         Fsu: Ultimate force of each reinforcement bar in the section [kN]
+        Fsd: Design force of each reinforcement bar in the section [kN]
         Fs: Resulting force of each reinforcement bar in the section due to fire [kN]
         εs_min: Minimum strain of the steel
         fcc: Concrete strength at 20C in [MPa]
@@ -15,7 +16,8 @@ FireSim made by Thomas Dyhr, DTU.BYG
         nHot: Stress Distribution Factor - HOT condition
         nCold: Stress Distribution Factor - COLD condition
     Returns:
-        Mstart: Positive moment capacity - Before Fire [kNm]
+        Md: Positive moment capacity - Design Value [kNm]
+        Mstart: Positive moment capacity - Start of Fire [kNm]
         Mhot: Positive moment capacity - HOT condition [kNm]
         Mcold: Positive moment capacity - COLD condition [kNm]
         Info: Check if cross-section is over-reinforced
@@ -31,18 +33,23 @@ import ghpythonlib.components as ghcomp
 ## Calculations ##
 
 FsuTOT = sum(Fsu)
+FsdTOT = sum(Fsd)
 FsTOT = sum(Fs)
 
-#Before Fire
-y0 = round(FsuTOT/(W*fcc)*1000,2) #Depth of compression zone
+#Design values
+yd = round(FsdTOT/(W*int(fcc))*1000,2) #Depth of compression zone
+Md = round(FsdTOT*(ds-yd/2)/1000,2) #Moment
+
+#Start of fire
+y0 = round(FsuTOT/(W*int(fcc))*1000,2) #Depth of compression zone
 Mstart = round(FsuTOT*(ds-y0/2)/1000,2) #Moment
 
 #During Fire
-y1 = round(FsTOT/(W*nHot*XicMHot*fcc)*1000,2) #Depth of compression zone
+y1 = round(FsTOT/(W*nHot*XicMHot*int(fcc))*1000,2) #Depth of compression zone
 Mhot = round(FsTOT*(ds-y1/2)/1000,2) #Moment
 
 #After Fire
-y2 = round(FsuTOT/(W*nCold*XicMCold*fcc)*1000,2) #Depth of compression zone
+y2 = round(FsuTOT/(W*nCold*XicMCold*int(fcc))*1000,2) #Depth of compression zone
 Mcold = round(FsuTOT*(ds-y2/2)/1000,2) #Moment
 
 
