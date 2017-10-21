45915877038f8d6ec71ce68ec3a744cb43a2d830
ThomasDyhr <thomasdyhr123@hotmail.com>
2017-10-21 10:40:30 +0200

    Testing how to manage data to and from an executable program - Not Working, version 001

---
+++ ReadExeProgram.py
@@ -0,0 +1,12 @@
+﻿import subprocess
+import os
+from subprocess import Popen, PIPE
+
+#if x == True:
+#    command = "ConFire"
+#    subprocess.Popen(command)
+#    subprocess.check_call(command , shell=True)
+
+
+p = Popen('ConFire', stdin=PIPE) #NOTE: no shell=True here
+p.communicate(os.linesep.join(["input 1", "input 2"]))
