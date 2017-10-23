bc84d61248cffef6ea9d694e14994cb9f3f9d520
ThomasDyhr <thomasdyhr123@hotmail.com>
2017-10-23 15:20:32 +0200

    Now working for moment calculations, version 002

--- LoadCalculation.py
+++ LoadCalculation.py
@@ -4,7 +4,7 @@ Applied load calculation. Calculates the moment and shear capacity of a beam bas
 FireSim made by Thomas Dyhr, DTU.BYG
 
     Args:
-        Loads: Applied load on beams [kN/m]
+        Loads: Applied load on beams [kN/m] or [kN]
         LoadType: Load type - (0=uniform distributed | 1=point load in middle or end(cantilever))
         Support: Support conditions - (0=Simply supported both ends | 1=Cantilever)
         Lengths: Length of beams [m]
@@ -19,12 +19,18 @@ ghenv.Component.Message = 'Load Calculation v.001'
 
 # Import classes
 
-
 ## Calculations ##
 
-#if Support == 0:
-#    if LoadType == 0:
-#        M = round(1/8*int(Loads)*(int(Lengths))^2)
+if Support == 0:
+    if LoadType == 0:
+        M = 1/8*Loads*Lengths*Lengths
+    elif LoadType == 1:
+        M = 1/4*Loads*Lengths
+elif Support == 1:
+    if LoadType == 0:
+        M = -1/2*Loads*Lengths*Lengths
+    elif LoadType == 1:
+        M = -Loads*Lengths
 
-print int(Loads)
-print float(Lengths)
+M = round(M,2)
+#V = round(V,2)
