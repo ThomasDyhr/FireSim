c2b3eb1c71e9954986e383f057c27cf82c9107f7
ThomasDyhr <thomasdyhr123@hotmail.com>
2017-10-27 13:16:16 +0200

    Code for sorting input data for shear reinforcement to a list of settings, version 001

---
+++ ShearRebarLayout.py
@@ -0,0 +1,31 @@
+﻿"""
+Shear Rebar Layout. Component that returns the settings for Shear Rebars
+-----
+FireSim made by Thomas Dyhr, DTU.BYG
+
+    Args:
+        Size: Diamter of rebars [mm]
+        Spacing: Spacing of shear rebars
+    Returns:
+        ShearBars: Shear rebar settings for the cross-section 
+"""
+
+ghenv.Component.Name = 'Shear Reinforcement'
+ghenv.Component.NickName = 'ShearRebars'
+ghenv.Component.Message = 'Shear Reinforcement v.001'
+
+## Code ##
+
+#Defaults
+defSize = 12
+defSpacing = 150
+
+if not Size:
+    Size = defSize
+if not Spacing:
+    Spacing = defSpacing
+
+#Output list
+MainBars = []
+MainBars.append(Size)
+MainBars.append(Spacing)
