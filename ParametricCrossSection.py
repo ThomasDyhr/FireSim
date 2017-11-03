9ecd0cbc04245bf056431403127f8f10dee4c50d
ThomasDyhr <thomasdyhr123@hotmail.com>
2017-11-03 14:29:03 +0100

    Updated with new outputs and changed the basepoint of the cross-section to be (0,0,0) in lower left corner - Still no fillets, version 004

--- ParametricCrossSection.py
+++ ParametricCrossSection.py
@@ -11,14 +11,17 @@ FireSim made by Thomas Dyhr, DTU.BYG
         ShearBars: Input settings for shear rebars in cross-section
         Cover: Cover thickness [mm] (From concrete edge to shear rebar's outer edge) 
     Returns:
-        Section: Concrete cross-section geometry
-        MainBars: Geometry of main reinforcement in top and bot
-        ShearBars: Geometry of shear reinforcement
+        Section: Cross-section geometry
+        AreaTop: Area of each rebar in the top layer [mm2]
+        xyTop: Local xy-coordinates of half of the rebars in top of the cross-section (Due to symmetry)
+        AreaBot: Area of each rebar in the bottom layer [mm2]
+        xyBot: Local xy-coordinates of half of the rebars in top of the cross-section (Due to symmetry)
+        ds: Distance from top/bottom edges to center of rebars [mm] (0 is for bottom bars - 1 is for top bars)
 """
 
 ghenv.Component.Name = 'CrossSection Geometry'
 ghenv.Component.NickName = 'SectionGeometry'
-ghenv.Component.Message = 'CrossSection Geometry v.003'
+ghenv.Component.Message = 'CrossSection Geometry v.004'
 
 #Import classes
 import rhinoscriptsyntax as rs
@@ -35,10 +38,14 @@ import rhinoscriptsyntax as rs
 #    Number = defNumber
 
 #Create concrete section
-ConSec = rs.AddRectangle( (-Width/2,0,0) , Width, Height)
+ConSec = rs.AddRectangle( (0,0,0) , Width, Height)
+explodeSec = rs.ExplodeCurves(ConSec)
+TopEdgeMid = rs.CurveMidPoint(explodeSec[2])
+BotEdgeMid = rs.CurveMidPoint(explodeSec[0])
 
 #Offsets for shear rebar position
-ShearOuter = rs.OffsetCurve(ConSec, [0,0,0] , Cover)
+ShearOuter = rs.OffsetCurve(ConSec, TopEdgeMid , Cover)
+
 #explode = rs.ExplodeCurves(ShearOuter)
 #radius = 20
 #fill = rs.CurveFilletPoints(explode[0],explode[1],radius)
@@ -48,18 +55,20 @@ ShearOuter = rs.OffsetCurve(ConSec, [0,0,0] , Cover)
 #fillet4 = rs.AddFilletCurve(explode[3],explode[0],radius)
 #fillet = [fillet1,fillet2,fillet3,fillet4]
 
+ShearInner = rs.OffsetCurve(ConSec, TopEdgeMid, Cover+ShearBars[0])
 
-ShearInner = rs.OffsetCurve(ConSec, [0,0,0], Cover+ShearBars[0])
+explodeInner = rs.ExplodeCurves(ShearInner)
 
 #Creating Top bars
-explodeInner = rs.ExplodeCurves(ShearInner)
-TopLine = rs.OffsetCurve(explodeInner[2], [0,0,0], RebarsTop[0]/2)
+TopLine = rs.OffsetCurve(explodeInner[2], BotEdgeMid, RebarsTop[0]/2)
 TopLine = rs.ExtendCurveLength(TopLine, 0, 2, -RebarsTop[0]/2)
 DivTop = rs.DivideCurve(TopLine,RebarsTop[1]-1)
 TopBars = []
 for i in range(len(DivTop)):
     TopBars.append(rs.AddCircle(DivTop[i],RebarsTop[0]/2))
 TopLineMid = rs.CurveMidPoint(TopLine)
+#ds to Top bars
+dsTop = rs.Distance(TopLineMid,BotEdgeMid)
 
 #Creating Bot bars
 BotLine = rs.OffsetCurve(explodeInner[0], TopLineMid, RebarsBot[0]/2)
@@ -68,7 +77,34 @@ DivBot = rs.DivideCurve(BotLine,RebarsBot[1]-1)
 BotBars = []
 for i in range(len(DivBot)):
     BotBars.append(rs.AddCircle(DivBot[i],RebarsBot[0]/2))
+BotLineMid = rs.CurveMidPoint(BotLine)
+#ds to bot bars
+dsBot = rs.Distance(BotLineMid,TopEdgeMid)
 
-Section = ConSec
-ShearBars = [ShearOuter[0],ShearInner[0]]
-MainBars = TopBars+BotBars
+#Output
+Section = [ConSec]
+for i in range(len(TopBars)):
+    Section.append(TopBars[i])
+for i in range(len(BotBars)):
+    Section.append(BotBars[i])
+Section.append(ShearOuter[0])
+Section.append(ShearInner[0])
+
+AreaTop = []
+for i in TopBars:
+    AreaTop.append(int(rs.Area(i)))
+
+xyTop = []
+DivTop.reverse()
+for i in range(int(round(len(DivTop)/2))):
+    xyTop.append(DivTop[i])
+
+AreaBot = []
+for i in BotBars:
+    AreaBot.append(int(rs.Area(i)))
+
+xyBot = []
+for i in range(int(round(len(DivBot)/2))):
+    xyBot.append(DivBot[i])
+
+ds = [dsBot,dsTop]
