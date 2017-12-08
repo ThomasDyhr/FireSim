630fc14a7b1a393babd72800ef01d2b872132363
ThomasDyhr <thomasdyhr123@hotmail.com>
2017-12-08 11:00:36 +0100

    Most recent version that now calculates reduced strengths for all bars, not using symmetry since this is not always the case, version 009

--- SteelStrength.py
+++ SteelStrength.py
@@ -8,19 +8,19 @@ FireSim made by Thomas Dyhr, DTU.BYG
         fyk: Characteristic steel strength at 20C in [MPa] - Default: 550
         Es: Steel E-modulus at 20C in [GPa] - Default: 210
         γm: Partial factor
-        ξsHOT: List of Deterioration factors for half of the reinforcement bars in the section due to symmetry - HOT condition
-        ξsCOLD: List of Deterioration factors for half of the reinforcement bars in the section due to symmetry - COLD condition
+        ξsHot: List of Deterioration factors - HOT condition
+        ξsCold: List of Deterioration factors - COLD condition
     Returns:
         Fsu: Ultimate force of each reinforcement bar in the section [kN]
         Fsd: Design force of each reinforcement bar in the section [kN]
-        FsHOT: Resulting force of each reinforcement bar in the section due to fire - HOT condition [kN]
-        FsCOLD: Resulting force of each reinforcement bar in the section due to fire - COLD condition [kN]
+        FsHot: Resulting force of each reinforcement bar in the section due to fire - HOT condition [kN]
+        FsCold: Resulting force of each reinforcement bar in the section due to fire - COLD condition [kN]
         εs_min: Minimum strain of the steel
 """
 
 ghenv.Component.Name = 'Steel Strength'
 ghenv.Component.NickName = 'SteelStr'
-ghenv.Component.Message = 'Steel Strength v.008'
+ghenv.Component.Message = 'Steel Strength v.009'
 
 # Import classes
 import ghpythonlib.components as ghcomp
@@ -45,22 +45,12 @@ Fsd = [a * int(fyk)/1000/float(γm) for a in A]
 #Determine if there is an odd or even number of rebars (modulus=0 means even)
 mod = ghcomp.Modulus(len(A),2)
 
-#Extend list of degredation factors to fit the number of rebars
-if mod==0:
-    XiHOT=ghcomp.InsertItems(ξsHOT,list(reversed(ξsHOT)),len(ξsHOT))
-    XiCOLD=ghcomp.InsertItems(ξsCOLD,list(reversed(ξsCOLD)),len(ξsCOLD))
-else:
-    XiHOT=ghcomp.InsertItems(ξsHOT,ξsHOT,len(ξsHOT))
-    XiHOT.pop()
-    XiCOLD=ghcomp.InsertItems(ξsCOLD,ξsCOLD,len(ξsCOLD))
-    XiCOLD.pop()
-
 # Multiply initial strength with degredation factor
-FsHOT = []
-FsCOLD = []
+FsHot = []
+FsCold = []
 for i in range(0, len(Fsu)):   
-     FsHOT.append(round(Fsu[i]*XiHOT[i],2))
-     FsCOLD.append(round(Fsu[i]*XiCOLD[i],2))
+     FsHot.append(round(Fsu[i]*ξsHot[i],2))
+     FsCold.append(round(Fsu[i]*ξsCold[i],2))
 
 #Minimum strain of steel
 εs_min = round(fyk/(Es*1000)*100+0.2,3)
