ae571ae027e0ded90a34ad8abdb0c663bd9d974a
ThomasDyhr <thomasdyhr123@hotmail.com>
2017-12-14 14:12:20 +0100

    BETA - Most reason, version 1.0

--- LoadCalculation.py
+++ LoadCalculation.py
@@ -4,46 +4,65 @@ Applied load calculation. Calculates the moment and shear forces in a beam based
 FireSim made by Thomas Dyhr, DTU.BYG
 
     Args:
-        Loads: Applied load on beams [kN/m] or [kN]
-        LoadType: Load type - (0=uniform distributed | 1=point load in middle or end(cantilever))
-        Support: Support conditions - (0=Simply supported both ends | 1=Cantilever)
-        Lengths: Length of beams [m]
+        Loads: Applied load on beams [kN/m] or [kN] - Default: 40
+        LoadType: Load type - (0=uniform distributed | 1=point load in middle or end(cantilever)) - Default: 0
+        Support: Support conditions - (0=Simply supported both ends | 1=Cantilever) - Default: 0
+        Lengths: Length of beams [m] - Default: 5
     Returns:
-        M: Resulting moment [kNm]
-        V: Resulting shear force [kN]
+        Mmax: Resulting moment [kNm]
+        Vmax: Resulting shear force [kN]
+        LoadSet: Load settings as list
 """
 
-ghenv.Component.Name = 'Load_Calculation'
+ghenv.Component.Name = 'Load Calculation'
 ghenv.Component.NickName = 'LoadCalc'
-ghenv.Component.Message = 'Load Calculation v.002'
+ghenv.Component.Message = 'Load Calculation v. 1.0'
 
 # Import classes
 
 ## Calculations ##
 
+# Defaults
+
+defLoads = 40
+defLoadType = 0 
+defSupport = 0
+defLengths = 5
+
+if not Loads:
+    Loads = defLoads
+if not LoadType:
+    LoadType = defLoadType
+if not Support:
+    Support = defSupport
+if not Lengths:
+    Lengths = defLengths
+
+
 #Max Moment force
 if Support == 0:
     if LoadType == 0:
-        M = 1/8*Loads*Lengths*Lengths
+        Mmax = 1/8*Loads*Lengths*Lengths
     elif LoadType == 1:
-        M = 1/4*Loads*Lengths
+        Mmax = 1/4*Loads*Lengths
 elif Support == 1:
     if LoadType == 0:
-        M = -1/2*Loads*Lengths*Lengths
+        Mmax = -1/2*Loads*Lengths*Lengths
     elif LoadType == 1:
-        M = -Loads*Lengths
+        Mmax = -Loads*Lengths
 
 #Max Shear force
 if Support == 0:
     if LoadType == 0:
-        V = 1/2*Loads*Lengths
+        Vmax = 1/2*Loads*Lengths
     elif LoadType == 1:
-        V = 1/2*Loads
+        Vmax = 1/2*Loads
 elif Support == 1:
     if LoadType == 0:
-        V = Loads*Lengths
+        Vmax = Loads*Lengths
     elif LoadType == 1:
-        V = Loads
+        Vmax = Loads
 
-M = round(M,2)
-V = round(V,2)
+Mmax = round(Mmax,2)
+Vmax = round(Vmax,2)
+LoadSet = [Loads , LoadType , Support]
